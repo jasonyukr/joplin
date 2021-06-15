2da5b88bd770a07a65fe297607053a57ad0c4e19
Jason (Jin-Hwan) Yu <jin-hwan.yu@oracle.com>
2021-06-15 23:03:08 +0900

    Use hack font in html imported docs

--- packages/lib/import-enex-html-gen.js
+++ packages/lib/import-enex-html-gen.js
@@ -129,7 +129,11 @@ function enexXmlToHtml_(stream, resources) {
 			} else if (htmlUtils.isSelfClosingTag(tagName)) {
 				section.lines.push(`<${tagName}${attributesStr}/>`);
 			} else {
-				section.lines.push(`<${tagName}${attributesStr}>`);
+                if (tagName == 'en-note') {
+                    section.lines.push(`<${tagName}${attributesStr} style="font-family:Hack,serif;font-size:12px">`);
+                } else {
+                    section.lines.push(`<${tagName}${attributesStr}>`);
+                }
 			}
 		});
 
