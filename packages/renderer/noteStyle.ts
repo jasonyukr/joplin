b76f085ab56351213d0a9e1b772afcb4986f0bf5
Jason (Jin-Hwan) Yu <jin-hwan.yu@oracle.com>
2021-06-18 10:18:12 +0900

    Use important mark for font

--- packages/renderer/noteStyle.ts
+++ packages/renderer/noteStyle.ts
@@ -17,7 +17,7 @@ export default function(theme: any) {
 		/* https://necolas.github.io/normalize.css/ */
 		html{line-height:1.15;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}
 		article,aside,footer,header,nav,section{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0;overflow:visible}
-		pre{font-family:'Fira Code',monospace,monospace;font-size:12px;border:1px solid ${theme.codeBorderColor};}a{background-color:transparent;-webkit-text-decoration-skip:objects}
+		pre{font-family:'Fira Code',monospace,monospace !important;font-size:12px !important;border:1px solid ${theme.codeBorderColor};}a{background-color:transparent;-webkit-text-decoration-skip:objects}
 		b,strong{font-weight:bolder}small{font-size:80%}img{border-style:none}
 
 		body {
@@ -26,8 +26,8 @@ export default function(theme: any) {
 			word-wrap: break-word;
 			line-height: ${theme.lineHeight};
 			background-color: ${theme.backgroundColor};
-			font-family: ${fontFamily};
-			font-size: 12px;
+			font-family: ${fontFamily} !important;
+			font-size: 12px !important;
 			padding-bottom: ${formatCssSize(theme.bodyPaddingBottom)};
 			padding-top: ${formatCssSize(theme.bodyPaddingTop)};
 		}
